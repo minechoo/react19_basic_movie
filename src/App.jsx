52b8f1e2d2e6245ac376940ac08817dcb3b695f3
tubi55 <tubi55@nate.com>
2025-05-24 09:42:41 +0900

    react-router-dom을 통해 각 컴포넌트에 url경에 따른 라우터 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import MovieDetail from "./pages/MovieDetail";
 import MovieList from "./pages/MovieList";
 
 function App() {
   return (
     <main className="theme-bg w-full h-screen">
-      <MovieList />
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/detail" element={<MovieDetail />} />
+      </Routes>
     </main>
   );
 }
---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,7 @@
+export default function MovieDetail() {
+  return (
+    <section>
+      <h1>Movie Detail Page</h1>
+    </section>
+  );
+}
