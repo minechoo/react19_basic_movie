ef4ae6bb61ab60e5c47e14c3b4aae7b54103634a
tubi55 <tubi55@nate.com>
2025-05-24 16:46:14 +0900

    Card컴포넌트에 아이콘으로 평점 출력

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
 
 export default function Card({ data }) {
+  const [Stars] = useState(Math.round((data.vote_average / 10) * 5));
+
   return (
     <article className="w-full">
       {/* 썸네일 */}
@@ -26,6 +30,16 @@ export default function Card({ data }) {
       <h2 className="text-2xl font-dongle text-[oklch(var(--theme-text))] font-[500] mt-3">
         <Link to={"/detail/" + data.id}>{data.title}</Link>
       </h2>
+
+      <p className="flex gap-1 text-xs text-[oklch(var(--point-color))]">
+        {Array(Stars)
+          .fill()
+          .map((_, idx) => (
+            <span key={idx}>
+              <FaStar />
+            </span>
+          ))}
+      </p>
     </article>
   );
 }
