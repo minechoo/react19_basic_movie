008ac1ea1a17e1a4e95be6847d12d203ad8ac4bd
tubi55 <tubi55@nate.com>
2025-05-24 11:42:15 +0900

    배경 이미지에 그라디언트 마스크 생성

--- src/components/BgDetail.jsx
+++ src/components/BgDetail.jsx
@@ -1,10 +1,12 @@
 export default function BgDetail({ Detail }) {
   return (
-    <div>
+    <div className="fixed top-0 left-0 opacity-50 size-full">
       <img
         src={`https://image.tmdb.org/t/p/w1280${Detail.backdrop_path}`}
         alt={Detail.title}
+        className="size-full object-cover"
       />
+      <div className="fixed size-full top-0 left-0 bg-gradient-to-b from-transparent to-[oklch(var(--theme-bg))]"></div>
     </div>
   );
 }
