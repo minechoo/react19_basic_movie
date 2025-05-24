1e15887e8bdc99d4bd008484e66b4a90eef3e43d
tubi55 <tubi55@nate.com>
2025-05-24 11:51:32 +0900

    MovieInfo에서 타이틀 영역 스타일링

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,10 +1,14 @@
 export default function MovieInfo({ Detail, Trailer }) {
   return (
-    <div className="relative">
-      <article>
+    <div className="relative w-9/12 h-full pr-20 theme-text flex flex-col">
+      <article className="relative flex flex-wrap content-end w-full gap-10">
         {/* 영화 타이틀 */}
-        <h1>{Detail?.title}</h1>
-        <h2>{Detail?.original_title}</h2>
+        <h1 className="w-full text-[5vmax] font-hanna leading-none">
+          {Detail?.title}
+        </h1>
+        <h2 className="w-full text-2xl leading-none font-hanna theme-text opacity-40">
+          {Detail?.original_title}
+        </h2>
 
         {/* 영화 기타 정보 */}
         <div>
