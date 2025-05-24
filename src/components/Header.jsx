3914b2a259c2765ece07bb788f1eca9e2b945dd2
tubi55 <tubi55@nate.com>
2025-05-24 10:50:26 +0900

    영화 기타 정보 출력

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="w-full px-[10vw] py-6 fixed top-0 left-0 z-100 flex justify-between">
+    <header className="w-full px-[10vw] py-6 fixed top-0 left-0 z-100">
       <h1 className="font-orbitron text-2xl theme-text font-[800] tracking-tighter leading-none">
         <Link to="/">DCODELAB</Link>
       </h1>
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,11 +1,50 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function MovieDetail() {
   const { id } = useParams();
-  console.log("클릭한 썸네일의 고유 영화 ID", id);
+
+  const [Detail, setDetail] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const BASE_URL = "https://api.themoviedb.org/3/movie";
+      const API_KEY = "4cf85a609f260b43cf0278ad12483b46";
+      const REQ_URL = `${BASE_URL}/${id}?api_key=${API_KEY}&language=ko-KR`;
+
+      try {
+        const res = await fetch(REQ_URL);
+        const data = await res.json();
+        console.log(data);
+        setDetail(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        console.log("complete");
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
   return (
     <section>
-      <h1>Movie Detail Page</h1>
+      {/* 영화 타이틀 */}
+      <h1>{Detail?.title}</h1>
+      <h2>{Detail?.original_title}</h2>
+
+      {/* 영화 기타 정보 */}
+      <div>
+        <ul>
+          {Detail?.genres.map((el) => (
+            <li key={el.id}>{el.name}</li>
+          ))}
+        </ul>
+
+        <span>{Detail?.release_date}</span>
+        <span>{Detail?.runtime}분</span>
+        <span>{Detail?.vote_average} / 10</span>
+      </div>
     </section>
   );
 }
